af5180708645bd112192a08d6bced11b008272b2
Lenovo-PC\admin <jenish.arvaan@gmail.com>
2018-03-27 18:45:37 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { TutorialPage } from '../pages/tutorial/tutorial';
+
 import { SlidesPage } from '../pages/slides/slides';
 
 @NgModule({
