f88e7c227c64b990f241c79d5b338da0689b194c
Lenovo-PC\admin <jenish.arvaan@gmail.com>
2018-03-27 18:39:35 +0530

    slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { SlidesPage } from '../pages/slides/slides';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = SlidesPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
+      // Okay, so the platform is ready and our plugins are a  vailable.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
